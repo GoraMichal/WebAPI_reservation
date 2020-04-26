5a8008021682e4b5e693ad040c3a0d51b3b381f7
Michał Góra <michalgorazs3@gmail.com>
2020-04-26 11:43:06 +0200

    Controller, Model, Views

---
+++ WebAPI_reservation/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAPI_reservation.Models;
+
+namespace WebAPI_reservation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookRepository repository = BookRepository.Current;
+
+        public ViewResult Index()
+        {
+            return View(repository.GetAll());
+        }
+
+        // GET: Home
+        public ActionResult Add(Book newBook)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.Add(newBook);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+
+        public ActionResult Remove(int id)
+        {
+            repository.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(Book upBook)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.Update(upBook);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
